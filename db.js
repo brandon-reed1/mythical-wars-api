660bf5abab8e78e09cb52d8703f21af704b2452d
save-time <savetime929@gmail.com>
2021-11-19 16:52:56 -0500

    Change connection from Client to Pool

--- db.js
+++ db.js
@@ -1,25 +1,24 @@
 require('dotenv').config();
 const Pool = require('pg-pool');
-const { Client } = require('pg');
 
-let client
+let db
 
 if (process.env.NODE_ENV === "production") {
-  client = new Client({
+  db = new Pool({
     connectionString: process.env.DATABASE_URL,
     ssl: {
       rejectUnauthorized: false
     }
   });
-  client.connect();
+  db.connect();
 } else {
-  client = new Pool({
+  db = new Pool({
     user: process.env.PG_USER,
     password: process.env.PG_PASSWORD,
     host: process.env.PG_HOST,
     database: process.env.PG_DATABASE,
     port: process.env.PG_PORT,
   });
-}
+};
 
-module.exports = client;
+module.exports = db;
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const client = require('./db');
+const db = require('./db');
 const PORT = process.env.PORT || 6000;
 
 //middleware
@@ -22,7 +22,7 @@ app.get("/", async (request, response) => {
 //get all fams
 app.get("/families", async (request, response) => {
   try {
-    const allFamilies = await client.query("SELECT * FROM families");
+    const allFamilies = await db.query("SELECT * FROM families");
     response.json(allFamilies.rows);
   } catch (err) {
     console.error(err.message);
@@ -33,7 +33,7 @@ app.get("/families", async (request, response) => {
 app.get("/tests/:id", async (request, response) => {
   try {
     const { id } = request.params;
-    const test = await client.query("SELECT * FROM tests WHERE id = $1", [
+    const test = await db.query("SELECT * FROM tests WHERE id = $1", [
       id
     ]);
     response.json(test.rows[0]);
@@ -46,7 +46,7 @@ app.get("/tests/:id", async (request, response) => {
 //not quite right
 app.get("/attempts", async (request, response) => {
   try {
-    const allAttempts = await client.query("SELECT * FROM attempts");
+    const allAttempts = await db.query("SELECT * FROM attempts");
     response.json(allAttempts.rows);
   } catch (err) {
     console.error(err.message);
@@ -57,7 +57,7 @@ app.get("/attempts", async (request, response) => {
 app.post("/user", async (request, response) => {
   try {
     const { user_name, current_mod } = request.body;
-    const newUser = await client.query(
+    const newUser = await db.query(
       "INSERT INTO users (user_name, current_mod) VALUES($1, $2) RETURNING *",
       [user_name, current_mod]
     );
@@ -71,7 +71,7 @@ app.post("/user", async (request, response) => {
 app.post("/families", async (request, response) => {
   try {
     const { mod, title, datasets } = request.body;
-    const newFamily = await client.query(
+    const newFamily = await db.query(
       "INSERT INTO families (mod, title, datasets) VALUES($1, $2, $3) RETURNING *",
       [mod, title, datasets]
     );
@@ -85,7 +85,7 @@ app.post("/families", async (request, response) => {
 app.post("/tests", async (request, response) => {
   try {
     const { test_name, instructions, expected, methods, family } = request.body;
-    const newTest = await client.query(
+    const newTest = await db.query(
       "INSERT INTO tests (test_name, instructions, expected, methods, family) VALUES($1, $2, $3, $4) RETURNING *",
       [test_name, instructions, expected, methods, family]
     );
@@ -99,7 +99,7 @@ app.post("/tests", async (request, response) => {
 app.post("/attempts", async (request, response) => {
   try {
     const { user_id, test_id, passing, code, result } = request.body;
-    const newAttempt = await client.query(
+    const newAttempt = await db.query(
       "INSERT INTO attempts (user_id, test_id, passing, code, result) VALUES($1, $2, $3, $4, $5) RETURNING *",
       [user_id, test_id, passing, code, result]
     );
@@ -114,7 +114,7 @@ app.put("/attempts/:id", async (request, response) => {
   try {
     const { id } = request.params;
     const { passing, code, result } = request.body;
-    const updateAttempt = await client.query(
+    const updateAttempt = await db.query(
       "UPDATE attempts SET passing = $1, code = $2, result = $3 WHERE id = $4",
       [passing, code, result, id]
     );
@@ -128,7 +128,7 @@ app.put("/attempts/:id", async (request, response) => {
 app.delete("/attempts/:id", async (request, response) => {
   try {
     const { id } = request.params;
-    const deleteAttempt = await client.query("DELETE FROM attempts WHERE id = $1", [
+    const deleteAttempt = await db.query("DELETE FROM attempts WHERE id = $1", [
       id
     ]);
     response.json("Attempt was deleted!");
