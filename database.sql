5eecb90a0f13a9ed252fedf3eb66b36e5ab39581
save-time <savetime929@gmail.com>
2021-11-19 12:55:16 -0500

    Add correct SQL commands to create db

--- database.sql
+++ database.sql
@@ -1,40 +1,126 @@
-CREATE DATABASE mythicals
+CREATE TABLE IF NOT EXISTS families (
+	id serial primary key,
+	mod int,
+	tests int[],
+	title text,
+	datasets text[]
+);
 
-CREATE TABLE IF NOT EXISTS public.families
-(
-    id bigint NOT NULL DEFAULT nextval('mythical_family_id_seq'::regclass),
-    mod bigint,
-    tests bigint[],
-    name character varying COLLATE pg_catalog."default",
-    dataset character varying[] COLLATE pg_catalog."default",
-    CONSTRAINT mythical_family_pkey PRIMARY KEY (id)
-)
+CREATE TABLE IF NOT EXISTS tests (
+	id serial primary key,
+  test_name text,
+	instructions text,
+	expected text,
+	methods text[],
+	family int
+);
 
-CREATE TABLE IF NOT EXISTS public.tests
-(
-    id bigint NOT NULL DEFAULT nextval('test_id_seq'::regclass),
-    description character varying COLLATE pg_catalog."default",
-    expected character varying COLLATE pg_catalog."default",
-    methods character varying[] COLLATE pg_catalog."default",
-    family bigint,
-    CONSTRAINT test_pkey PRIMARY KEY (id)
-)
+CREATE TABLE IF NOT EXISTS users (
+	id serial primary key,
+	user_name text,
+	current_mod int
+);
 
-CREATE TABLE IF NOT EXISTS public.users
-(
-    id bigint NOT NULL DEFAULT nextval('users_id_seq'::regclass),
-    name character varying COLLATE pg_catalog."default" NOT NULL,
-    current_mod bigint,
-    CONSTRAINT users_pkey PRIMARY KEY (id)
-)
+CREATE TABLE IF NOT EXISTS attempts (
+	id serial primary key,
+	user_id int,
+	test_id int,
+	passing boolean,
+	code text,
+  result text
+);
 
-CREATE TABLE IF NOT EXISTS public.attempts
-(
-    id bigint NOT NULL DEFAULT nextval('attempts_id_seq'::regclass),
-    passing boolean,
-    code character varying COLLATE pg_catalog."default",
-    output character varying COLLATE pg_catalog."default",
-    user_id bigint,
-    test_id bigint,
-    CONSTRAINT attempts_pkey PRIMARY KEY (id)
-)
+INSERT INTO users (
+  user_name,
+  current_mod
+) VALUES (
+  'Alex Kio',
+  5
+);
+
+INSERT INTO families (
+  mod,
+	tests,
+	title,
+	datasets
+) VALUES (
+  2,
+  '{1, 2}',
+  'Kitties',
+  '{"const kitties = [
+    {
+      name: `Felicia`,
+      age: 2,
+      color: `grey`
+    },
+    {
+      name: `Tiger`,
+      age: 5,
+      color: `orange`
+    },
+    {
+      name: `Snickers`,
+      age: 8,
+      color: `orange`
+    },
+    {
+      name: `Max`,
+      age: 1,
+      color: `tuxedo`
+    }
+  ]"}'
+);
+
+INSERT INTO tests (
+  test_name,
+	expected,
+	methods,
+	family
+) VALUES (
+  'orangeKittyNames',
+  '[`Tiger`, `Snickers`]',
+  '{`filter`, `reduce`}',
+  1
+);
+
+INSERT INTO tests (
+  test_name,
+	expected,
+	methods,
+	family
+) VALUES (
+  'sortByAge',
+  '[{
+    name: `Snickers`,
+    age: 8,
+    color: `orange`
+  }, {
+    name: `Tiger`,
+    age: 5,
+    color: `orange`
+  }, {
+    name: `Felicia`,
+    age: 2,
+    color: `grey`
+  }, {
+    name: `Max`,
+    age: 1,
+    color: `tuxedo`
+  }]',
+  '{`sort`}',
+  1
+);
+
+INSERT INTO attempts (
+  user_id,
+	test_id,
+	passing,
+	code,
+  result
+) VALUES (
+  1,
+  1,
+  true,
+  'testing',
+  'not bad'
+);
