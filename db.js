7f772a7cf8659494627351febb5f194493d7ab5e
save-time <savetime929@gmail.com>
2021-11-19 16:37:13 -0500

    Refactor conditional postgres login

--- db.js
+++ db.js
@@ -1,31 +1,26 @@
 require('dotenv').config();
 const Pool = require('pg-pool');
-const url = require('url')
+const { Client } = require('pg');
+// const url = require('url')
 
-const devConfig = {
-  user: process.env.PG_USER,
-  password: process.env.PG_PASSWORD,
-  host: process.env.PG_HOST,
-  database: process.env.PG_DATABASE,
-  port: process.env.PG_PORT,
-};
+let client
 
-const params = url.parse(process.env.DATABASE_URL);
-const auth = params.auth.split(':');
+if (process.env.NODE_ENV === "production") {
+  client = new Client({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false
+    }
+  });
+  client.connect();
+} else {
+  client = new Pool({
+    user: process.env.PG_USER,
+    password: process.env.PG_PASSWORD,
+    host: process.env.PG_HOST,
+    database: process.env.PG_DATABASE,
+    port: process.env.PG_PORT,
+  });
+}
 
-const proConfig = {
-  user: auth[0],
-  password: auth[1],
-  host: params.hostname,
-  port: params.port,
-  database: params.pathname.split('/')[1],
-  ssl: true
-};
-
-// const proConfig = process.env.DATABASE_URL;
-
-const pool = new Pool(
-  process.env.NODE_ENV === "production" ? proConfig : devConfig
-);
-
-module.exports = pool;
+module.exports = client;
